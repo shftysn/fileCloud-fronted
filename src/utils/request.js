7d5ff6580d2fddfd9595f9480d24abf7b0cbd67c
shuo <shuo90237@gmail.com>
2026-04-11 10:29:52 +0800

    修改request.js中的变量名

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import store from '../store';
 import { clearAuth, setAccessToken } from '../store/authSlice';
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE-API-BASE-URL || '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 60000,
   withCredentials: true,
 });
